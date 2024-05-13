3a636bd56bf16911019cf1c667b29612ce6ebbed
OcampoEmiliano <ocampoemilianopa@gmail.com>
2024-05-13 12:36:16 -0300

    parOImpar.js modificado

--- assets/js/parOImpar.js
+++ assets/js/parOImpar.js
@@ -1,8 +1,7 @@
-let num = [0,1,2,3,4,5,6,7,8,9,10];
 
-for (let index = 0; index < num.length; index++) {
-    console.log(num[index]);
-    if ((num[index] % 2) == 0) {
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if ((i % 2) == 0) {
         console.log("par")
     } else {
         console.log("impar")
